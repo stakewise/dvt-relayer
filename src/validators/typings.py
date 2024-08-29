4034476be415d9c891c1d3fec4d6cb956074956c
Evgeny Gusarov <evgeny@stakewise.io>
2024-08-29 14:39:36 +0300

    Review fix 2

--- src/validators/typings.py
+++ src/validators/typings.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, field
-from typing import Union
 
 from eth_typing import BlockNumber, BLSSignature, HexStr
 
@@ -8,6 +7,12 @@ from eth_typing import BlockNumber, BLSSignature, HexStr
 class NetworkValidator:
     public_key: HexStr
     block_number: BlockNumber
+
+
+@dataclass
+class OraclesExitSignatureShares:
+    public_keys: list[HexStr]
+    encrypted_exit_signatures: list[HexStr]
 
 
 @dataclass
@@ -21,10 +26,4 @@ class Validator:
     exit_signature_shares: dict[int, BLSSignature] = field(default_factory=dict)
 
     # Oracles' shares
-    oracles_exit_signature_shares: Union['OraclesExitSignatureShares', None] = None
-
-
-@dataclass
-class OraclesExitSignatureShares:
-    public_keys: list[HexStr]
-    encrypted_exit_signatures: list[HexStr]
+    oracles_exit_signature_shares: OraclesExitSignatureShares | None = None
