1acfa07607b3889fcc9dbd2343ec350d9ec4b4ff
Evgeny Gusarov <evgeny@stakewise.io>
2026-03-16 12:36:46 +0300

    Fix lint

--- src/validators/schema.py
+++ src/validators/schema.py
@@ -35,8 +35,7 @@ class SignatureShareRequest(BaseModel):
 # End of signature shares request
 
 
-class SignatureShareResponse(BaseModel):
-    ...
+class SignatureShareResponse(BaseModel): ...
 
 
 # Validators data consumed by the DVT Sidecars to sign deposit messages and exit messages
