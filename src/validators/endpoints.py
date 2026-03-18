80d680f0c9601430881bf7267d65017a0a982835
Evgeny Gusarov <evgeny@stakewise.io>
2026-03-18 13:54:20 +0300

    Save signature shares even if signature is ready

--- src/validators/endpoints.py
+++ src/validators/endpoints.py
@@ -41,65 +41,66 @@ async def submit_signature_shares(
         if validator is None:
             continue
 
-        # Handle exit signature shares
-        if validator.exit_signature is None and not validator.exit_signature_shares.get(
-            request.share_index
-        ):
+        # Save exit signature share if not already saved
+        if not validator.exit_signature_shares.get(request.share_index):
             validator.exit_signature_shares[request.share_index] = BLSSignature(
                 Web3.to_bytes(hexstr=share.exit_signature)
             )
-
-            if len(validator.exit_signature_shares) >= settings.signature_threshold:
-                # Reconstruct and validate exit signature
-                exit_signature = reconstruct_shared_bls_signature(validator.exit_signature_shares)
-                if not validate_exit_signature(
-                    validator.public_key, validator.validator_index, exit_signature
-                ):
-                    raise HTTPException(
-                        status_code=400,
-                        detail=(
-                            f'invalid exit signature for public_key={share.public_key},'
-                            f' share_index={request.share_index}'
-                        ),
-                    )
-
-                validator.exit_signature = exit_signature
-
-                # Split exit signature into shares for oracles
-                oracles_shares = await get_oracles_exit_signature_shares(
-                    public_key=validator.public_key,
-                    validator_index=validator.validator_index,
-                    exit_signature=validator.exit_signature,
-                )
-                validator.oracles_exit_signature_shares = oracles_shares
-
-        # Handle deposit signature shares
-        if validator.deposit_signature is None and not validator.deposit_signature_shares.get(
-            request.share_index
-        ):
+        # Save deposit signature share if not already saved
+        if not validator.deposit_signature_shares.get(request.share_index):
             validator.deposit_signature_shares[request.share_index] = BLSSignature(
                 Web3.to_bytes(hexstr=share.deposit_signature)
             )
 
-            if len(validator.deposit_signature_shares) >= settings.signature_threshold:
-                # Reconstruct and validate deposit signature
-                deposit_signature = reconstruct_shared_bls_signature(
-                    validator.deposit_signature_shares
+        # Handle exit signature shares
+        if (
+            validator.exit_signature is None
+            and len(validator.exit_signature_shares) >= settings.signature_threshold
+        ):
+            # Reconstruct and validate exit signature
+            exit_signature = reconstruct_shared_bls_signature(validator.exit_signature_shares)
+            if not validate_exit_signature(
+                validator.public_key, validator.validator_index, exit_signature
+            ):
+                raise HTTPException(
+                    status_code=400,
+                    detail=(
+                        f'invalid exit signature for public_key={share.public_key},'
+                        f' share_index={request.share_index}'
+                    ),
                 )
-                if not validate_deposit_signature(
-                    validator.public_key,
-                    Web3.to_bytes(hexstr=validator.withdrawal_credentials),
-                    validator.amount,
-                    deposit_signature,
-                ):
-                    raise HTTPException(
-                        status_code=400,
-                        detail=(
-                            f'invalid deposit signature for public_key={share.public_key},'
-                            f' share_index={request.share_index}'
-                        ),
-                    )
 
-                validator.deposit_signature = deposit_signature
+            validator.exit_signature = exit_signature
+
+            # Split exit signature into shares for oracles
+            oracles_shares = await get_oracles_exit_signature_shares(
+                public_key=validator.public_key,
+                validator_index=validator.validator_index,
+                exit_signature=validator.exit_signature,
+            )
+            validator.oracles_exit_signature_shares = oracles_shares
+
+        # Handle deposit signature shares
+        if (
+            validator.deposit_signature is None
+            and len(validator.deposit_signature_shares) >= settings.signature_threshold
+        ):
+            # Reconstruct and validate deposit signature
+            deposit_signature = reconstruct_shared_bls_signature(validator.deposit_signature_shares)
+            if not validate_deposit_signature(
+                validator.public_key,
+                Web3.to_bytes(hexstr=validator.withdrawal_credentials),
+                validator.amount,
+                deposit_signature,
+            ):
+                raise HTTPException(
+                    status_code=400,
+                    detail=(
+                        f'invalid deposit signature for public_key={share.public_key},'
+                        f' share_index={request.share_index}'
+                    ),
+                )
+
+            validator.deposit_signature = deposit_signature
 
     return SignatureShareResponse()
